7e3fd100152e4a6aa563defc0a523fcb889a33b3
Turya Biswas <mydelllaptop233@gmail.com>
2026-02-15 20:58:34 +0600

    secured gapi key

--- dromos/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dromos/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import file_selector_macos
 import flutter_local_notifications
 import geolocator_apple
@@ -12,6 +13,7 @@ import package_info_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
